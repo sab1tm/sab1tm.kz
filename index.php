07fe0deb759806a7e98de42853ffb3e6b6262b40
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2025-05-15 23:13:28 +0500

    use php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+    <head>
+        <?php include "/parts/head.php"; ?>
+        <title>Dev's Bay</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <h1>Dev's Bay</h1>
+            </div>
+            <div class="menu">
+                <a href="/index.php">HOME</a>
+                <a href="/notes.php">NOTES</a>
+            </div>
+        </div>
+    </body>
+</html>
