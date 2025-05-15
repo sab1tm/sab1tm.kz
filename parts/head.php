00db42d6e38d83edca0399200ae242ae4c9600a7
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2025-05-15 23:24:24 +0500

    add css

--- parts/head.php
+++ parts/head.php
@@ -3,26 +3,7 @@ echo '
 
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-<style>
-    ::root {
-        --bg: black;
-        --primary-color: palegreen;
-    }
-    * {
-        background-color: val(--bg);
-        color: val(--primary-color);
-    }
-    .header {
-        height: 200px;
-        width: 100%;
-        border: 1px solid val(--primary-color);
-    }
-    .menu {
-        width: 100%;
-        border: 1px solid val(--primary-color);
-    }
-</style>
+<link rel="stylesheet" href="../css/styles.css">
 
 ';
 
