07552dc8b549ec3ffbde2c7f7827920d4041c7b7
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2025-05-18 00:08:21 +0500

    add login

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include "module/security.php"; ?>
+<!doctype html>
+<html>
+    <head>
+        <?php include "template/head.php"; ?>
+        <title>Dev's Bay</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="login">
+                <form action="login.php" method="post">
+			          <input type="text" name="login" placeholder="Логин">
+			          <br><br>
+			          <input type="password" name="password" placeholder="Пароль">
+			          <br><br>
+			          <button type="submit" name="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
