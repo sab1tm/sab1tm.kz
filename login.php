2285598345a40ee73d03fce933ddd497fc35bb6e
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2025-05-18 00:11:56 +0500

    login style

--- login.php
+++ login.php
@@ -5,16 +5,14 @@
         <title>Dev's Bay</title>
     </head>
     <body>
-        <div class="container">
-            <div class="login">
-                <form action="login.php" method="post">
-			          <input type="text" name="login" placeholder="Логин">
-			          <br><br>
-			          <input type="password" name="password" placeholder="Пароль">
-			          <br><br>
-			          <button type="submit" name="submit">Login</button>
-                </form>
-            </div>
+        <div class="login-form">
+            <form action="login.php" method="post">
+			    <input type="text" name="login" placeholder="Логин">
+			    <br><br>
+			    <input type="password" name="password" placeholder="Пароль">
+			    <br><br>
+			    <button type="submit" name="submit">Login</button>
+            </form>
         </div>
     </body>
 </html>
