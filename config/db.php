8b502e6cd7876f5928dc19ea554196c30b9dabed
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2025-05-19 02:03:00 +0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 
-// $link = mysqli_connect("", "sabtmkz_main", "1!Edp96r7", "sabtmkz_main");
-$link = mysqli_connect("", "root", "", "sabtmkz");
+$link = mysqli_connect("", "sabtmkz_main", "1!Edp96r7", "sabtmkz_main");
+// $link = mysqli_connect("", "root", "", "sabtmkz");
 
 if (!$link) {
     printf("Can't connect to DB. Error: %s\n", mysqli_connect_error());
