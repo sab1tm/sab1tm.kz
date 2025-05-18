146b936fefd4deed28a9c1bc3d9da3dbe2baa99c
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2025-05-19 02:22:00 +0500

    Update login.php

--- login.php
+++ login.php
@@ -14,7 +14,7 @@ include "service/auth_login.php"; ?>
             <form action="login.php" method="post">
                 <input type="text" name="login" placeholder="login"> <br>
                 <input type="password" name="password" placeholder="password"> <br><br>
-                <button type="submit" name="submit">Login</button>
+                <button type="submit" name="submit">[enter]</button>
             </form>
         </div>
     </body>
